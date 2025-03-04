d8ba33ac91a96ae287a4147255dfd2fd6da52e24
kinatigre <kinatigre0@gmail.com>
2025-03-04 20:52:51 +0100

    teste change

---
+++ test/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('scroll', function() {
+    const scrollToTopButton = document.getElementById('scrollToTop');
+    if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop - 80,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
